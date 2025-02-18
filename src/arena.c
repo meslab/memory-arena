70ba75ac9792218dd17dd60e0f0f1151a3a04185
Anton Sidorov <antosid@gmail.com>
2025-02-18 20:51:34 +0000

    -

--- src/arena.c
+++ src/arena.c
@@ -1,34 +1,32 @@
-#include "arena.h"
-#include <stdlib.h>
+#include "../include/arena.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void arena_init(MemoryArena *arena, size_t size) {
-    arena->buffer = (uint8_t *)malloc(size);
-    if (arena->buffer == NULL) {
-        perror("Failed to allocate memory for arena");
-        exit(EXIT_FAILURE);
-    }
-    arena->size = size;
-    arena->offset = 0;
+  arena->buffer = (uint8_t *)malloc(size);
+  if (arena->buffer == NULL) {
+    perror("Failed to allocate memory for arena");
+    exit(EXIT_FAILURE);
+  }
+  arena->size = size;
+  arena->offset = 0;
 }
 
 void *arena_alloc(MemoryArena *arena, size_t size) {
-    if (arena->offset + size > arena->size) {
-        fprintf(stderr, "Out of memory in arena\n");
-        exit(EXIT_FAILURE);
-    }
-    void *ptr = &arena->buffer[arena->offset];
-    arena->offset += size;
-    return ptr;
+  if (arena->offset + size > arena->size) {
+    fprintf(stderr, "Out of memory in arena\n");
+    exit(EXIT_FAILURE);
+  }
+  void *ptr = &arena->buffer[arena->offset];
+  arena->offset += size;
+  return ptr;
 }
 
-void arena_reset(MemoryArena *arena) {
-    arena->offset = 0;
-}
+void arena_reset(MemoryArena *arena) { arena->offset = 0; }
 
 void arena_free(MemoryArena *arena) {
-    free(arena->buffer);
-    arena->buffer = NULL;
-    arena->size = 0;
-    arena->offset = 0;
+  free(arena->buffer);
+  arena->buffer = NULL;
+  arena->size = 0;
+  arena->offset = 0;
 }
