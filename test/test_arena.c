3b4cdd0d0e8255cc85ec5a69c0cf7fb18a2e7a5a
Anton Sidorov <antosid@gmail.com>
2025-02-28 17:17:11 +0000

    -

--- test/test_arena.c
+++ test/test_arena.c
@@ -51,6 +51,7 @@ int main(int argc, char *argv[]) {
   test_string_unicode_from(&arena, "你好");
   test_string_unicode_from(&arena, "مرحبًا");
   test_string_unicode_from(&arena, "سلام");
+  test_string_unicode_from(&arena, "Γειά σου");
   test_string_unicode_from(&arena, "здравствуй юникод");
   arena_debug(&arena);
 
